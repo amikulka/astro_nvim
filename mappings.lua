ffce0073b51cb6e4dcda190e97125a8671811832
Aaron Mikulka <aaronjmikulka@gmail.com>
2024-01-04 08:09:09 -0800

    messing with some mappings and cheatsheet

--- mappings.lua
+++ mappings.lua
@@ -40,7 +40,7 @@ return {
     ["n"] = { "nzzzv" }, -- goes to next match and centers screen
 
     ["<leader>y"] = { '"+y', desc = "Copy to system clipboard" },
-    ["<leader>Y"] = { '"+Y', desc = "Copy rest of line to system clipboard" },
+    ["<leader>Y"] = { '"+Y', desc = "Copy line to system clipboard" },
 
     ["<leader>d"] = { '"_d', desc = "Delete without yanking" },
     ["<leader>D"] = { '"_D', desc = "Delete rest of line without yanking" },
@@ -66,9 +66,13 @@ return {
 
     ["<leader>pm"] = false,
     ["<leader>pM"] = false,
+    ["<leader>h"] = false,
+
     ["<leader> pp"] = { '"+p', desc = "Paste from system clipboard" },
 
     ["<leader>?"] = { function() require("cheatsheet").show_cheatsheet() end, desc = "Open cheatsheet" },
+
+    ["<D-v"] = { '"+p', desc = "Paste from system clipboard" },
   },
   v = {
     ["J"] = { ":m '>+1<CR>gv=gv" },
@@ -76,6 +80,8 @@ return {
 
     ["<leader>y"] = { '"+y', desc = "Copy to system clipboard" },
     ["<leader>d"] = { '"_d', desc = "Delete without yanking" },
+    ["<D-c"] = { '"+y', desc = "Copy to system clipboard" },
+    ["<D-v"] = { '"+p', desc = "Paste from system clipboard" },
   },
   t = {
     -- setting a mapping to false will disable it
@@ -84,5 +90,62 @@ return {
   },
   x = {
     ["<leader>pk"] = { '"_dp', desc = "Don't loose yank when pasting" },
+    ["<D-v"] = { '"+p', desc = "Paste from system clipboard" },
   },
 }
+--   maps.n["<leader>d"] = sections.d
+--   maps.v["<leader>d"] = sections.d
+--   -- modified function keys found with `showkey -a` in the terminal to get key code
+--   -- run `nvim -V3log +quit` and search through the "Terminal info" in the `log` file for the correct keyname
+--   maps.n["<F5>"] = { function() require("dap").continue() end, desc = "Debugger: Start" }
+--   maps.n["<F17>"] = { function() require("dap").terminate() end, desc = "Debugger: Stop" } -- Shift+F5
+--   maps.n["<F21>"] = { -- Shift+F9
+--     function()
+--       vim.ui.input({ prompt = "Condition: " }, function(condition)
+--         if condition then require("dap").set_breakpoint(condition) end
+--       end)
+--     end,
+--     desc = "Debugger: Conditional Breakpoint",
+--   }
+--   maps.n["<F29>"] = { function() require("dap").restart_frame() end, desc = "Debugger: Restart" } -- Control+F5
+--   maps.n["<F6>"] = { function() require("dap").pause() end, desc = "Debugger: Pause" }
+--   maps.n["<F9>"] = { function() require("dap").toggle_breakpoint() end, desc = "Debugger: Toggle Breakpoint" }
+--   maps.n["<F10>"] = { function() require("dap").step_over() end, desc = "Debugger: Step Over" }
+--   maps.n["<F11>"] = { function() require("dap").step_into() end, desc = "Debugger: Step Into" }
+--   maps.n["<F23>"] = { function() require("dap").step_out() end, desc = "Debugger: Step Out" } -- Shift
+--   maps.n["<leader>db"] = { function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint (F9)" }
+--   maps.n["<leader>dB"] = { function() require("dap").clear_breakpoints() end, desc = "Clear Breakpoints" }
+--   maps.n["<leader>dc"] = { function() require("dap").continue() end, desc = "Start/Continue (F5)" }
+--   maps.n["<leader>dC"] = {
+--     function()
+--       vim.ui.input({ prompt = "Condition: " }, function(condition)
+--         if condition then require("dap").set_breakpoint(condition) end
+--       end)
+--     end,
+--     desc = "Conditional Breakpoint (S-F9)",
+--   }
+--   maps.n["<leader>di"] = { function() require("dap").step_into() end, desc = "Step Into (F11)" }
+--   maps.n["<leader>do"] = { function() require("dap").step_over() end, desc = "Step Over (F10)" }
+--   maps.n["<leader>dO"] = { function() require("dap").step_out() end, desc = "Step Out (S-F11)" }
+--   maps.n["<leader>dq"] = { function() require("dap").close() end, desc = "Close Session" }
+--   maps.n["<leader>dQ"] = { function() require("dap").terminate() end, desc = "Terminate Session (S-F5)" }
+--   maps.n["<leader>dp"] = { function() require("dap").pause() end, desc = "Pause (F6)" }
+--   maps.n["<leader>dr"] = { function() require("dap").restart_frame() end, desc = "Restart (C-F5)" }
+--   maps.n["<leader>dR"] = { function() require("dap").repl.toggle() end, desc = "Toggle REPL" }
+--   maps.n["<leader>ds"] = { function() require("dap").run_to_cursor() end, desc = "Run To Cursor" }
+--
+--   if is_available "nvim-dap-ui" then
+--     maps.n["<leader>dE"] = {
+--       function()
+--         vim.ui.input({ prompt = "Expression: " }, function(expr)
+--           if expr then require("dapui").eval(expr, { enter = true }) end
+--         end)
+--       end,
+--       desc = "Evaluate Input",
+--     }
+--     maps.v["<leader>dE"] = { function() require("dapui").eval() end, desc = "Evaluate Input" }
+--     maps.n["<leader>du"] = { function() require("dapui").toggle() end, desc = "Toggle Debugger UI" }
+--     maps.n["<leader>dh"] = { function() require("dap.ui.widgets").hover() end, desc = "Debugger Hover" }
+--   end
+-- end
+-- d = { desc = get_icon("Debugger", 1, true) .. "Debugger" },
--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -30,14 +30,24 @@ return {
         swap = {
           swap_next = {
             ["<leader>na"] = { "@parameter.inner", desc = "Swap parameter/argument with next" },
-            ["<leader>nm"] = { "@function.outer" },
+            ["<leader>nm"] = { "@function.outer", desc = "Swap method/function with next" },
           },
           swap_previous = {
-            ["<leader>pa"] = "@parameter.inner",
-            ["<leader>pm"] = "@function.outer",
+            ["<leader>pa"] = { "@parameter.inner", desc = "Swap parameter/argument with last" },
+            ["<leader>pm"] = { "@function.outer", desc = "Swap method/function with next" },
           },
         },
       },
     }
+
+    local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
+
+    vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+    vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+
+    vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
+    vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
+    vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
+    vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
   end,
 }
